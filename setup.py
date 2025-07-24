3586ccfd775f5970360773d1ab64c74babd3f73f
SuhelAnsari9272 <faizalsuhel874742@gmail.com>
2025-07-24 07:20:00 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Suhel Ansari",
+    author_email="s.ansari09272@gmail.com",
+    packages=find_packages()
+)
